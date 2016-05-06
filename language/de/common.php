e446bde3ea05ea5064ad43b481c891866e19a801
tas2580 <mail@tas2580.net>
2016-05-05 20:44:32 -0400

    Applied fixes from StyleCI

--- language/de/common.php
+++ language/de/common.php
@@ -1,23 +1,17 @@
 <?php
 /**
- *
- * @package phpBB Extension - tas2580 AJAX Notifications
  * @copyright (c) 2016 tas2580 (https://tas2580.net)
  * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
- *
  */
-
-if (!defined('IN_PHPBB'))
-{
-	exit;
+if (!defined('IN_PHPBB')) {
+    exit;
 }
 
-if (empty($lang) || !is_array($lang))
-{
-	$lang = array();
+if (empty($lang) || !is_array($lang)) {
+    $lang = [];
 }
 
-$lang = array_merge($lang, array(
-	'ACP_AJAXNOTIFICATION_TIMEOUT'				=> 'Timeout für Ajax Benachrichtigungen',
-	'ACP_AJAXNOTIFICATION_TIMEOUT_EXPLAIN'		=> 'Gib in Sekunden an wie oft nach neuen Benachrichtigungen geschaut werden soll. Beachte das eine zu niedrige Zahl den Server sehr stark belasten kann.',
-));
+$lang = array_merge($lang, [
+    'ACP_AJAXNOTIFICATION_TIMEOUT'                => 'Timeout für Ajax Benachrichtigungen',
+    'ACP_AJAXNOTIFICATION_TIMEOUT_EXPLAIN'        => 'Gib in Sekunden an wie oft nach neuen Benachrichtigungen geschaut werden soll. Beachte das eine zu niedrige Zahl den Server sehr stark belasten kann.',
+]);
